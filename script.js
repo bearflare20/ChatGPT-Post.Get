5e6c1ba5261a3e2abc74b0de86b5f862c1bbb2e5
bearflare20 <71741919+bearflare20@users.noreply.github.com>
2023-03-23 15:23:29 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
-// Toggle between GET and POST options
-function toggleOptions(option) {
-  if (option === "get") {
-    document.getElementById("get-btn").classList.add("active");
-    document.getElementById("post-btn").classList.remove("active");
-    document.getElementById("post-options").style.display = "none";
-  } else if (option === "post") {
-    document.getElementById("post-btn").classList.add("active");
-    document.getElementById("get-btn").classList.remove("active");
-    document.getElementById("post-options").style.display = "block";
+const switchBtn = document.querySelector('.switch');
+const postContainer = document.querySelector('.post-container');
+
+switchBtn.addEventListener('click', () => {
+  if (document.getElementById('post').checked) {
+    postContainer.classList.remove('hidden');
+  } else {
+    postContainer.classList.add('hidden');
   }
-}
+});
+
+const form = document.querySelector('form');
+const responseContainer = document.getElementById('response');
+const copyBtn = document.getElementById('copy-btn');
+const clearBtn = document.getElementById('clear-btn');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const xhr = new XMLHttpRequest();
+  xhr.open(form.querySelector('input[name="request-type"]:checked').value, form.url.value);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.onload = function () {
+    responseContainer.innerHTML = xhr.response;
+  };
+  xhr.onerror = function () {
+    responseContainer.innerHTML = 'Error: Request failed';
+  };
+  xhr.send(form.querySelector('#payload').value);
+});
+
+copyBtn.addEventListener('click', () => {
+  const range = document.createRange();
+  range.selectNode(document.getElementById('response'));
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(range);
+  document.execCommand('copy');
+});
+
+clearBtn.addEventListener('click', () => {
+  responseContainer.innerHTML = '';
+});
